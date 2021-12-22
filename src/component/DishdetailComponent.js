d4abdfecbe66ca9c61318fcb971e9824f669ef61
minos95 <rahal.aminos@gmail.com>
2021-12-22 23:44:25 +0100

    add dichdetails and menu and dishes file

---
+++ src/component/DishdetailComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class DishDetail extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div>HI</div>;
+  }
+}
+
+export default DishDetail;
